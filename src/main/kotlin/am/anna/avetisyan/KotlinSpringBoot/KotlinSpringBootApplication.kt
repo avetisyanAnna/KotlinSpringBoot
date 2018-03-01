ced425bdec6771e3e5a98b3efc0733eb9a221702
Anna Avetisyan <anna.avetisyan@iunetworks.am>
2018-03-01 09:46:03 +0400

    first commit

---
+++ src/main/kotlin/am/anna/avetisyan/KotlinSpringBoot/KotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package am.anna.avetisyan.KotlinSpringBoot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinSpringBootApplication::class.java, *args)
+}
